f573a411f3f5df083a83e3e8fed0f5534f0b2de9
iitbhilai23 <psureshkumar@iitbhilai.ac.in>
2026-07-27 17:57:07 +0530

    add vtp policy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/vt'>
+    <BrowserRouter basename='/vtp'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
